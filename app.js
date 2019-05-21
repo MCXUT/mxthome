a2b10244e07083823af79c01a34b754cccd6c8c4
slee <home@slees-MacBook-Air.local>
2019-05-21 15:42:04 -0400

    First Update

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const path = require("path");
+const session = require("express-session");
+
+const passport = require("./config/passport_config");
+const keys = require("./config/keys");
+const app = express();
+
+const userRoutes = require("./routes/users");
+
+// Connect to database
+mongoose.connect("mongodb+srv://" + keys.mongodb.user + ":" + keys.mongodb.pass + "@cluster0-gdoa3.mongodb.net/test?retryWrites=true", {useNewUrlParser: true});
+const db = mongoose.connection;
+
+// set view engine
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+// Set bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// session setting
+app.use(session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+}));
+
+// passport setting
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.use("/auth", userRoutes);
+
+// Set port and listen to it
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+    console.log("Server started on port " + app.get('port'));
+});
