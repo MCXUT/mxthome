4cb9967d3ab169d00b4859bafcd7417bd3808d8c
slee <home@slees-MacBook-Air.local>
2019-05-27 18:18:32 -0400

    Fix - modalSetting.js: remove id "outside" navbar.ejs: fix id

--- public/scripts/modalSetting.js
+++ public/scripts/modalSetting.js
@@ -1,20 +1,36 @@
-document.getElementById("auth").addEventListener("click", () => {
-    document.querySelector(".bg-modal").style.display = "flex";
+document.getElementById("auth").addEventListener("click", (e) => {
+    document.querySelector("#cocoaModal").style.display = "block";
 });
 
-document.querySelector(".close").addEventListener("click", () => {
-    document.querySelector(".bg-modal").style.display = "none";
+document.querySelector(".close").addEventListener("click", (e) => {
+    document.querySelector("#cocoaModal").style.display = "none";
 });
 
-document.getElementById("outside").addEventListener("click", (e) => {
-  if(e.target.className == "bg-modal sticky-top") {
-    // e.preventDefault();
-     document.querySelector(".bg-modal").style.display = "none";
-  }
+document.querySelector("#close1").addEventListener("click", (e) => {
+    console.log(e);
+    document.querySelector("#jungohModal").style.display = "none";
+});
+
+
+// document.getElementById("outside").addEventListener("click", (e) => {
+//   console.log(e);
+//   if(e.target.idName == "modal.in.modal_site_login.ui-draggable") {
+//     // e.preventDefault();
+//      document.querySelector("#cocoaModal").style.display = "none";
+//   }
+//
+document.getElementById("signup").addEventListener("click", () => {
+    document.querySelector("#cocoaModal").style.display = "none";
+    document.querySelector("#jungohModal").style.display = "block";
+});
+
+  // document.getElementById("signup").addEventListener("click", () => {
+  //     document.querySelector("#register").style.display = "flex";
+  // });
+
   // else{
   //
   // }
-});
 //
 // window.onclick = function (event) {
 //   if (document.querySelector(".bg-modal").style.display === "flex") {
